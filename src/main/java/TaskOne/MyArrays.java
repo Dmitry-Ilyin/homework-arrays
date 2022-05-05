b9b757f1449648cd7283755beca2a601e90a3033
Dmitry Ilyin <dmitry.ilyin95@gmail.com>
2022-05-05 17:53:57 +0300

    fix

--- src/main/java/TaskOne/MyArrays.java
+++ src/main/java/TaskOne/MyArrays.java
@@ -8,30 +8,39 @@ public class MyArrays {
     public static void main(String[] args) {
         int[] array = new Random().ints(20, -10, 11).limit(20).toArray();
         System.out.println(Arrays.toString(array));
-        int max = Arrays.stream(array).max().orElseThrow(NoSuchElementException::new);
-        System.out.println("Максимальный элемент : " + max);
-        int min = Arrays.stream(array).min().orElseThrow(NoSuchElementException::new);
-        System.out.println("Минимальный элемент : " + min);
-        int indexMaxElement = 0;
-        int indexMinElement = 0;
+        int minPositive = Arrays.stream(array)
+                .filter(i -> i > 0)
+                .min()
+                .orElseThrow(NoSuchElementException::new);
+        System.out.println("Минимальный положительный элемент : " + minPositive);
+
+        int maxNegative = Arrays.stream(array)
+                .filter(i -> i < 0)
+                .max()
+                .orElseThrow(NoSuchElementException::new);
+        System.out.println("Максимальный отрицательный элемент : " + maxNegative);
+
+        int indexMinPositiveElement = 0;
+        int indexMaxNegativeElement = 0;
+
         for (int i = 0; i < array.length; i++) {
-            if (array[i] == max) {
-                indexMaxElement = i;
-                System.out.println("Индекс максимального элемента : " + indexMaxElement);
+            if (array[i] == minPositive) {
+                indexMinPositiveElement = i;
+                System.out.println("Индекс минимального положительного элемента : " + indexMinPositiveElement);
                 break;
             }
         }
 
         for (int i = 0; i < array.length; i++) {
-            if (array[i] == min) {
-                indexMinElement = i;
-                System.out.println("Индекс минимального элемента : " + indexMinElement);
+            if (array[i] == maxNegative) {
+                indexMaxNegativeElement = i;
+                System.out.println("Индекс максимального отрицательного элемента : " + indexMaxNegativeElement);
                 break;
             }
         }
-        int temp = array[indexMaxElement];
-        array[indexMaxElement] = array[indexMinElement];
-        array[indexMinElement] = temp;
+
+        array[indexMinPositiveElement] = maxNegative;
+        array[indexMaxNegativeElement] = minPositive;
         System.out.println(Arrays.toString(array));
     }
 }
